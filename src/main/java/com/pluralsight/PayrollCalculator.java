09e5ffcd175e3ceaff42cbfa936abf8c9dbd59fa
Christian Crites <ccrites98@gmail.com>
2023-10-05 13:43:37 -0400

    PayrollCalculator

---
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+import java.util.*;
+public class PayrollCalculator {
+    public static double main(String[] args) {
+
+        Scanner keyBoard = new Scanner(System.in);
+        //name input
+        System.out.print("What is your name? ");
+        String name = keyBoard.nextLine();
+        //age input
+        System.out.print("How many hours did you work? ");
+        String hours = keyBoard.nextLine();
+        // Float numbers to calculate inputted hours
+        
+
+
+    }
+}
